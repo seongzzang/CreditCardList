2166d952fc8bf8b99908561cc8ed91fae7ec5182
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-09-08 23:50:30 +0900

    update_cardList

---
+++ CreditCardList/CardListCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardListCell.swift
+//  CreditCardList
+//
+//  Created by 양성혜 on 2022/09/08.
+//
+
+import UIKit
+
+class CardListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
