6861efc8738acaab5db99b41b8d92d7257efdcf4
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-09-20 22:58:58 +0900

    update_cardListcell

--- CreditCardList/CardListViewController.swift
+++ CreditCardList/CardListViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class CardListViewController: UITableViewController {
+    
+    var creditCardList : [CreditCard] = []
+    
     override func viewDidLoad() {
         viewDidLoad()
+        
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell",for: indexPath) as? CardListCell else {return UITableViewCell()}
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        return cell
     }
 }
