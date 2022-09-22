021627736e9702a3ef1b450a513bc4a8a1f9b086
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-09-22 23:31:48 +0900

    update_cardDetailView

---
+++ CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by 양성혜 on 2022/09/22.
+//
+
+import Foundation
+import UIKit
+
+class CardDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
